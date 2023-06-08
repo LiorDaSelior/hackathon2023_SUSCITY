fd6d395f255c86d9bcf2b5e9782d7b5300377301
nofarshl <nofarshlomo@mail.tau.ac.il>
2023-06-09 02:21:57 +0300

    add profile page

--- city/src/component/PersonalDetails.tsx
+++ city/src/component/PersonalDetails.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import logo from './Tuti.jpeg';
+import "./PersonalDetails.css"
 
 const PersonalDetails = () => {
     const x = 0;
     return(
-        <div>
-        <div className ="nofar">Nofar</div>
+        <div className='div'>
+        <div className ="nofar"></div>
+        <p className="headline">Welcome Nofar!</p>
+        <img src={logo} alt="Logo" className="rounded-image" />
+        <p className='Tscore'>Total coins: 100</p>
+        <p className='Ascore'>Available coins: 50</p>
+        {/* <image></image>
         <button>the queen</button>
-        <div>{x}</div>
+        <div>{x}</div> */}
         </div>
     )
 }
